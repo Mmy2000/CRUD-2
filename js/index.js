301d417c392f6455c5209fe9a407c967e45eae06
Mmy2000 <yahianada005@gmail.com>
2024-03-18 06:47:59 +0200

    creating add product function

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ var productsContainer = [];
 
 function addProduct() {
     var product = {
-        code:productNameInput.value,
-        price:productPriceInput.value,
-        category:productCategoryInput.value,
-        desc:productDescInput.value,
-        image:'img/post-3.jpg',
+        code: productNameInput.value,
+        price: productPriceInput.value,
+        category: productCategoryInput.value,
+        desc: productDescInput.value,
+        image: 'img/post-3.jpg',
     }
     productsContainer.push(product);
     clearForm();
@@ -32,18 +32,18 @@ function displayProduct() {
     var cartoona = ``;
     for (var i = 0; i < productsContainer.length; i++) {
         cartoona += `<div class="col-md-3">
-        <div class="inner p-2">
-                    <img src="img/post-3.jpg" class="w-100" alt="">
-                    <div class="content p-2">
-                        <h2>Sumsung</h2>
-                        <span>2000$</span>
-                        <span class="badge bg-secondary ms-4">Category</span>
-                        <p class="mt-2">Lorem ipsum dolor sit.</p>
-                        <a href="#"  class="btn btn btn-outline-warning">Update</a>
-                        <a href="#"  class="btn btn btn-outline-danger">Delete</a>
-                    </div>
-                </div> 
-                </div>`
+                        <div class="inner p-2">
+                            <img src="img/post-3.jpg" class="w-100" alt="">
+                            <div class="content p-2">
+                                <h2>Sumsung</h2>
+                                <span>2000$</span>
+                                <span class="badge bg-secondary ms-4">Category</span>
+                                <p class="mt-2">Lorem ipsum dolor sit.</p>
+                                <a href="#"  class="btn btn btn-outline-warning">Update</a>
+                                <a href="#"  class="btn btn btn-outline-danger">Delete</a>
+                            </div>
+                        </div> 
+                    </div>`
     }
     document.getElementById("rowData").innerHTML = cartoona;
 }
