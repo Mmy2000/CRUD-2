588f73e17d267a7f5747dcd7d7092a8938c4c2bd
Mmy2000 <yahianada005@gmail.com>
2024-03-19 06:07:51 +0200

    updating

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ var productPriceInput = document.getElementById("productPrice");
 var productCategoryInput = document.getElementById("productCat");
 var productDescInput = document.getElementById("productDesc");
 var productImageInput = document.getElementById("productImage");
-var productsContainer = [];
+var productsContainer ;
 
 if (localStorage.getItem('products') !== null) {
     productsContainer =JSON.parse(localStorage.getItem('products'));
-    displayProduct();
+    displayProduct(productsContainer);
+}else{
+    productsContainer = [];
 }
 
 function addProduct() {
@@ -21,7 +23,7 @@ function addProduct() {
     productsContainer.push(product);
     localStorage.setItem('products' , JSON.stringify(productsContainer));
     clearForm();
-    displayProduct();
+    displayProduct(productsContainer);
 
 }
 
@@ -33,17 +35,17 @@ function clearForm() {
     productImageInput.value = null;
 }
 
-function displayProduct() {
+function displayProduct(arr) {
     var cartoona = ``;
-    for (var i = 0; i < productsContainer.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         cartoona += `<div class="col-md-3">
                         <div class="inner p-2">
                             <img src="img/post-3.jpg" class="w-100" alt="">
                             <div class="content p-2">
-                                <h2>${productsContainer[i].code}</h2>
-                                <span>${productsContainer[i].price}$</span>
-                                <span class="badge bg-secondary ms-4">${productsContainer[i].category}</span>
-                                <p class="mt-2">${productsContainer[i].desc}</p>
+                                <h2>${arr[i].code}</h2>
+                                <span>${arr[i].price}$</span>
+                                <span class="badge bg-secondary ms-4">${arr[i].category}</span>
+                                <p class="mt-2">${arr[i].desc}</p>
                                 <a href="#"  class="btn btn btn-outline-warning w-100 mb-2">Update<i class="fa-solid fa-pen ps-1"></i></a>
                                 <a href="#" onclick="deleteProduct(${i})"  class="btn btn btn-outline-danger w-100">Delete<i class="fa-solid fa-trash-can ps-1"></i></a>
                             </div>
@@ -55,5 +57,5 @@ function displayProduct() {
 function deleteProduct(index) {
     productsContainer.splice(index,1)
     localStorage.setItem('products' , JSON.stringify(productsContainer));
-    displayProduct();
+    displayProduct(productsContainer);
 }
